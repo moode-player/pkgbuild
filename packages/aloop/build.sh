99c2e993c32dce0ecfde534f5e3a857c0da34638
bitkeeper <bitkeeper@users.noreply.github.com>
2022-01-08 12:17:06 +0000

    [aloop] updated to use new rbl_dkms_prepare helper function

--- packages/aloop/build.sh
+++ packages/aloop/build.sh
@@ -10,42 +10,26 @@
 
 . ../../scripts/rebuilder.lib.sh
 
-KERNEL_VER=`uname -r | sed -r "s/([0-9.]*)[-].*/\1/"`
+KERNEL_VER=$(rbl_get_current_kernel_version)
 
 PKG="aloop_$KERNEL_VER-1"
 
+# required for creating a dkms project:
 DKMS_MODULE="aloop/0.2"
 SRC_DIR="aloop-0.2"
 ARCHS=( v7l+ v7+ )
 MODULE="snd-aloop.ko"
 MODULE_PATH='sound/drivers'
 
-_rbl_decode_pkg_version
-_rbl_check_curr_is_package_dir
-_rbl_cleanup_previous_build
-_rbl_change_to_build_root
-
-rbl_check_build_dep dkms
-rbl_check_build_dep rpi-source
-# this will download the kernel source if needed and set the ENV KERNEL_SOURCE_ARCHIVE to the location of the tarball
-rbl_get_kernel_source
+rbl_dkms_prepare intree
 
 #------------------------------------------------------------
 # Custom part of the packing
 
-# 1. setup project files required for dkms:
-echo $BUILD_ROOT_DIR/source/$SRC_DIR
-mkdir -p $BUILD_ROOT_DIR/source/$SRC_DIR
-
-cp $PKGBUILD_ROOT/scripts/templates/deb_dkms/dkms-patchmodule.intree.sh $BUILD_ROOT_DIR/source/$SRC_DIR/dkms-patchmodule.sh
-chmod +x $BUILD_ROOT_DIR/source/$SRC_DIR/*.sh
-rbl_dkms_apply_template $PKGBUILD_ROOT/scripts/templates/deb_dkms/dkms.conf $BUILD_ROOT_DIR/source/$SRC_DIR/dkms.conf
-cp $BASE_DIR/*.patch $BUILD_ROOT_DIR/source/$SRC_DIR
-
-# 2. build the modules with dkms:
+# 1. build the modules with dkms:
 dkms build --dkmstree $BUILD_ROOT_DIR --sourcetree $BUILD_ROOT_DIR/source -k $KERNEL_VER-v7l+ -k $KERNEL_VER-v7+ $DKMS_MODULE
 
-# 3. packed it with fpm:
+# 2. packed it with fpm:
 # (wanted multiple arch deb which isn't possible with dkms and wanted to prevent install deps of dkms and related)
 
 # create deb postinstall and afterremove scripts based on template:
