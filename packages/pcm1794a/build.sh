3e05e066c96ce1104762bdd65beb0f15c84240b0
bitkeeper <bitkeeper@users.noreply.github.com>
2022-01-08 12:16:53 +0000

    [pcm1794a] updated to use new rbl_dkms_prepare helper function

--- packages/pcm1794a/build.sh
+++ packages/pcm1794a/build.sh
@@ -10,39 +10,24 @@
 
 . ../../scripts/rebuilder.lib.sh
 
-KERNEL_VER=`uname -r | sed -r "s/([0-9.]*)[-].*/\1/"`
+KERNEL_VER=$(rbl_get_current_kernel_version)
 
 PKG="pcm1794a_$KERNEL_VER-1"
 
+# required for creating a dkms project:
 DKMS_MODULE="pcm1794a/0.2"
 SRC_DIR="pcm1794a-0.2"
 ARCHS=( v7l+ v7+ )
 MODULE="snd-soc-pcm1794a.ko"
 MODULE_PATH='sound/soc/codecs'
 
-_rbl_decode_pkg_version
-_rbl_check_curr_is_package_dir
-_rbl_cleanup_previous_build
-_rbl_change_to_build_root
-
-rbl_check_build_dep dkms
-rbl_check_build_dep rpi-source
-# this will download the kernel source if needed and set the ENV KERNEL_SOURCE_ARCHIVE to the location of the tarball
-rbl_get_kernel_source
+rbl_dkms_prepare intree
 
 #------------------------------------------------------------
 # Custom part of the packing
 
-# 1. setup project files required for dkms:
-echo $BUILD_ROOT_DIR/source/$SRC_DIR
-mkdir -p $BUILD_ROOT_DIR/source/$SRC_DIR
 
-cp $PKGBUILD_ROOT/scripts/templates/deb_dkms/dkms-patchmodule.intree.sh $BUILD_ROOT_DIR/source/$SRC_DIR/dkms-patchmodule.sh
-chmod +x $BUILD_ROOT_DIR/source/$SRC_DIR/*.sh
-rbl_dkms_apply_template $PKGBUILD_ROOT/scripts/templates/deb_dkms/dkms.conf $BUILD_ROOT_DIR/source/$SRC_DIR/dkms.conf
-cp $BASE_DIR/*.patch $BUILD_ROOT_DIR/source/$SRC_DIR
-
-# 2. build the modules with dkms:
+# 1. build the modules with dkms:
 dkms build --dkmstree $BUILD_ROOT_DIR --sourcetree $BUILD_ROOT_DIR/source -k $KERNEL_VER-v7l+ -k $KERNEL_VER-v7+ $DKMS_MODULE
 
 # examples of other dkms outputs:
@@ -57,14 +42,12 @@ dkms build --dkmstree $BUILD_ROOT_DIR --sourcetree $BUILD_ROOT_DIR/source -k $KE
 # dkms mktarball --dkmstree $BUILD_ROOT_DIR --binaries-only -k $KERNEL_VER-v7l+ -k $KERNEL_VER-v7+ $DKMS_MODULE
 
 
-# 3. packed it with fpm:
+# 2. packed it with fpm:
 # (wanted multiple arch deb which isn't possible with dkms and wanted to prevent install deps of dkms and related)
-
 
 # create deb postinstall and afterremove scripts based on template:
 rbl_dkms_apply_template $PKGBUILD_ROOT/scripts/templates/deb_dkms/postinstall.sh $BUILD_ROOT_DIR/postinstall.sh
 rbl_dkms_apply_template $PKGBUILD_ROOT/scripts/templates/deb_dkms/afterremove.sh $BUILD_ROOT_DIR/afterremove.sh
-
 
 # place build modules in the correct file tree
 rbl_dkms_grab_modules
