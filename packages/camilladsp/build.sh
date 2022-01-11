15b84019943c21268cd78f3ca4b7edee7c6ad102
bitkeeper <bitkeeper@users.noreply.github.com>
2022-01-11 19:24:20 +0000

    [camilladsp] changed install dir from /usr/bin to /usr/local/bin
    moode and the alsacdsp module expect camilladsp in this location.

--- packages/camilladsp/build.sh
+++ packages/camilladsp/build.sh
@@ -2,25 +2,28 @@
 
 . ../../scripts/rebuilder.lib.sh
 
-PKG="camilladsp_0.6.3-1moode1"
+PKG="camilladsp_0.6.3-1moode2"
 
 PKG_SOURCE_GIT="https://github.com/HEnquist/camilladsp.git"
 PKG_SOURCE_GIT_TAG="v0.6.3"
 
 rbl_check_cargo
 rbl_prepare_clone_from_git $PKG_SOURCE_GIT $PKG_SOURCE_GIT_TAG
-rbl_create_git_archive $PKG_SOURCE_GIT_TAG ../${PKGNAME}_${PKGVERSION}.orig.tar.gz
+rbl_create_git_archive $PKG_SOURCE_GIT_TAG $BUILD_ROOT_DIR/${PKGNAME}_${PKGVERSION}.orig.tar.gz
 
 # ------------------------------------------------------------
 # Custom part of the packing
 
-# install deps
+# Install deps
 rbl_check_build_dep libasound2-dev
 
-#Add to [package.metadata.deb] section of Cargo.toml:
-echo "[package.metadata.deb]" >> Cargo.toml
-echo "revision=\"$DEBVER$DEBLOC\"" >> Cargo.toml
+# Set install location to /usr/local/bin
+patch -p1 < $BASE_DIR/camilladsp_cargo-deb.patch
 
+# Add to [package.metadata.deb] section of Cargo.toml:
+echo "" >> Cargo.toml
+echo "revision=\"$DEBVER$DEBLOC\"" >> Cargo.toml
+# echo "" >> Cargo.toml
 
 # Build it for arch with neon support
 RUSTFLAGS='-C target-feature=+neon -C target-cpu=native' cargo-deb -- --no-default-features --features alsa-backend --features websocket
