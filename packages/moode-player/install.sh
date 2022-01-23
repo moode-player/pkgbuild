6ba7d513942a2021ddd6b82d9f66d95ddf04b9ec
bitkeeper <bitkeeper@users.noreply.github.com>
2022-01-23 20:55:41 +0100

    [moode-player|install.sh] lock also mpd, mpc and caps packages after install

--- packages/moode-player/install.sh
+++ packages/moode-player/install.sh
@@ -18,9 +18,9 @@ sudo reboot
 
 # step 3 - Install moode audio player
 KERNEL_VER=$(uname -r | sed -r "s/([0-9.]*)[-].*/\1/")
-sudo apt-mark hold raspberrypi-kernel
 echo "installing drivers for kernel version: ${KERNEL_VER}"
 # it can happen that the kernel modules aren't already avaible for the current kernel:
 sudo apt install moode-player aloop-$KERNEL_VER pcm1794a-$KERNEL_VER ax88179-$KERNEL_VER rtl88xxau-$KERNEL_VER
+sudo apt-mark hold raspberrypi-kernel mpd mpc caps
 sudo reboot
 
