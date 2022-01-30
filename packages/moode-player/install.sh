71917253ccf110f16200cae7578d87e7fa0c8a05
bitkeeper <bitkeeper@users.noreply.github.com>
2022-01-30 11:37:08 +0100

    [moode-player] updated install script with new repo and lock of packages

--- packages/moode-player/install.sh
+++ packages/moode-player/install.sh
@@ -7,7 +7,7 @@
 
 # step 1 - Install moode repo
 curl -1sLf \
-'https://dl.cloudsmith.io/public/moodeaudio/m8x/setup.deb.sh' \
+'https://dl.cloudsmith.io/public/moodeaudio/m8y/setup.deb.sh' \
 | sudo -E bash
 
 # step 2 - Make sure system is up2date
@@ -21,6 +21,6 @@ KERNEL_VER=$(uname -r | sed -r "s/([0-9.]*)[-].*/\1/")
 echo "installing drivers for kernel version: ${KERNEL_VER}"
 # it can happen that the kernel modules aren't already avaible for the current kernel:
 sudo apt install moode-player aloop-$KERNEL_VER pcm1794a-$KERNEL_VER ax88179-$KERNEL_VER rtl88xxau-$KERNEL_VER
-sudo apt-mark hold raspberrypi-kernel mpd mpc caps
+sudo moode-apt-mark hold
 sudo reboot
 
