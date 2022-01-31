4f2d59cfd70756bd763568f4c8cfa3d0e8b27abb
bitkeeper <bitkeeper@users.noreply.github.com>
2022-01-31 19:30:57 +0100

    [squeezelite] add new package squeezelite with additional options RPI and GPIO

---
+++ packages/squeezelite/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#########################################################################
+#
+# Build recipe for patched squeezelite with RPI and GPIO options
+#
+# (C) bitkeeper 2022 http://moodeaudio.org
+# License: GPLv3
+#
+#########################################################################
+
+. ../../scripts/rebuilder.lib.sh
+
+
+PKG_DSC_URL="http://deb.debian.org/debian/pool/main/s/squeezelite/squeezelite_1.9+git20210102.78fef68-3.dsc"
+
+rbl_prepare_from_dsc_url $PKG_DSC_URL
+
+#------------------------------------------------------------
+# Custom part of the packing
+
+# patch and add patch to debian
+patch -p1 < $BASE_DIR/debian.rules.patch
+
+# set debian local suffix flag
+DEBFULLNAME=$DEBFULLNAME DEBEMAIL=$DEBEMAIL dch --local $DEBSUFFIX "Rebuild for moOde bullseye with RPI and GPIO options."
+
+#------------------------------------------------------------
+
+rbl_build
+echo "done"
