e96296282bb3d7e69c5aa14f40d0816a9e5dcb5b
bitkeeper <bitkeeper@users.noreply.github.com>
2022-02-08 11:06:58 +0100

    [moode-player] update installation documentation install.sh

--- packages/moode-player/install.sh
+++ packages/moode-player/install.sh
@@ -17,10 +17,22 @@ sudo apt upgrade
 sudo reboot
 
 # step 3 - Install moode audio player
-KERNEL_VER=$(uname -r | sed -r "s/([0-9.]*)[-].*/\1/")
+
+echo "install moode audio player"
+sudo apt install moode-player
+
 echo "installing drivers for kernel version: ${KERNEL_VER}"
+SUPPORTED_KERNELS=(5.10.63 5.10.92)
+KERNEL_VER=$(uname -r | sed -r "s/([0-9.]*)[-].*/\1/")
+inarray=$(echo ${SUPPORTED_KERNELS[@]} | grep -o "$KERNEL_VER" | wc -w)
 # it can happen that the kernel modules aren't already avaible for the current kernel:
-sudo apt install moode-player aloop-$KERNEL_VER pcm1794a-$KERNEL_VER ax88179-$KERNEL_VER rtl88xxau-$KERNEL_VER
+if [ $inarray -eq 1 ]
+then
+    sudo apt install aloop-$KERNEL_VER pcm1794a-$KERNEL_VER ax88179-$KERNEL_VER rtl88xxau-$KERNEL_VER
+else
+    echo "Warning unsupported kernel; skipping driver install."
+fi
+
 sudo moode-apt-mark hold
 sudo reboot
 
