7b2f51441abb2301e2718adf18a2ef81c1bcfc3f
Tim Curtis <tim@moodeaudio.org>
2022-02-27 11:43:27 -0500

    Update comment header and text plus minor bugfix
    - Fix placement of KERNEL_VER assignment

--- packages/moode-player/install.sh
+++ packages/moode-player/install.sh
@@ -1,38 +1,49 @@
 
+#########################################################################
 #
-# install moode audio player on a clean raspberry OS bullseye lite
+# Recipe for installing moOde audio player on clean RaspiOS Lite system
+# RaspiOS Lite Bullseye release is required.
 #
+# (C) bitkeeper 2021 http://moodeaudio.org
+# License: GPLv3
+#
+#########################################################################
 
-# step 0 - Prep a Debian Bullseye image on SD card
+# Step 0 - Write RaspiOS image to SD card
 
-# step 1 - Install moode repo
+# Step 1 - Configure APT for moOde package repo
 curl -1sLf \
 'https://dl.cloudsmith.io/public/moodeaudio/m8y/setup.deb.sh' \
 | sudo -E bash
 
-# step 2 - Make sure system is up2date
+# Step 2 - Make sure OS is up to date
 sudo apt update
 sudo apt upgrade
 
+# Reboot in case upgrade installs new kernel
 sudo reboot
 
-# step 3 - Install moode audio player
+# Step 3 - Install moOde audio player
 
-echo "install moode audio player"
+echo "Install moOde audio player"
 sudo apt install moode-player
 
-echo "installing drivers for kernel version: ${KERNEL_VER}"
-SUPPORTED_KERNELS=(5.10.63 5.10.92)
+# Step 4 - Install kernel drivers
+
 KERNEL_VER=$(uname -r | sed -r "s/([0-9.]*)[-].*/\1/")
+echo "Checking for drivers built for kernel version: ${KERNEL_VER}"
+# NOTE: Be sure to update this array
+SUPPORTED_KERNELS=(5.10.63 5.10.92 5.15.23)
 inarray=$(echo ${SUPPORTED_KERNELS[@]} | grep -o "$KERNEL_VER" | wc -w)
-# it can happen that the kernel modules aren't already avaible for the current kernel:
 if [ $inarray -eq 1 ]
 then
+    echo "Installing kernel drivers"
     sudo apt install aloop-$KERNEL_VER pcm1794a-$KERNEL_VER ax88179-$KERNEL_VER rtl88xxau-$KERNEL_VER
 else
-    echo "Warning unsupported kernel; skipping driver install."
+    echo "Warning: drivers have not yet been built for this kernel version"
+    echo "Skipping driver install"
 fi
 
+# Apply package hold protection
 sudo moode-apt-mark hold
 sudo reboot
-
