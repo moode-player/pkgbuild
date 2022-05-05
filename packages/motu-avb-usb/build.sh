9592f53b9d8f011850a312bd656d504a1ba833be
bitkeeper <bitkeeper@users.noreply.github.com>
2022-05-05 21:01:47 +0100

    [motu-avb] Add package for motu-avb
    Default not required to be installed. Only required in case of trouble, especially with simultaneously capture and playback of the same device.

---
+++ packages/motu-avb-usb/build.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+#########################################################################
+#
+# Build recipe for the motu avb usb driver package from Drumfix
+#
+# (C) bitkeeper 2021 http://moodeaudio.org
+# License: GPLv3
+#
+# https://github.com/Drumfix/motu-avb-usb
+#
+#########################################################################
+
+
+. ../../scripts/rebuilder.lib.sh
+
+KERNEL_VER=$(rbl_get_current_kernel_version)
+
+PKG="motu-avb-usb_1.0-1"
+
+PKG_SOURCE_GIT="https://github.com/Drumfix/motu-avb-usb.git"
+PKG_SOURCE_GIT_TAG="1f0e0a0ec19b25baaf2d83808047067e7cd947ae"
+
+
+# required for creating a dkms project:
+# DKMS_MODULE="ax88179_178a/2.0"
+# SRC_DIR="ax88179_178a-2.0"
+# ARCHS=( v7l+ v7+ )
+# MODULE="ax88179_178a.ko"
+# MODULE_PATH='drivers/net/usb'
+if [ $ARCH64 -eq 1 ]
+then
+  ARCHS=( v8+ )
+else
+  ARCHS=( v7l+ v7+ )
+fi
+MODULE="motu.ko"
+
+
+# allo source with module replacement:
+# SOURCE_GIT="https://github.com/Drumfix/motu-avb-usb.git"
+# SOURCE_GIT_TAG="master"
+
+# rbl_dkms_prepare outree
+
+rbl_check_fpm
+rbl_prepare_clone_from_git $PKG_SOURCE_GIT
+rbl_create_git_archive $PKG_SOURCE_GIT_TAG ../${PKGNAME}_${PKGVERSION}.orig.tar.gz
+rbl_check_kernel_headers
+
+DKMS_MODULE="$PKGNAME/$PKGVERSION"
+# MODULE_PATH='net/wireless/realtek/rtlwifi'
+
+cd $BUILD_ROOT_DIR
+
+#------------------------------------------------------------
+# Custom part of the packing
+
+# 1. build the modules with dkms:
+
+# use kernel headers (stock kernel) or source (rpi-update/custom kernel)
+if [ $MODULE_BUILD_USE_SOURCE -eq 1 ]
+then
+  echo 'PRE_BUILD="dkms-patchmodule.sh %MODULE_PATH%"' >> $BUILD_ROOT_DIR/$PKGNAME-$PKGVERSION/dkms.conf
+  cp $PKGBUILD_ROOT/scripts/templates/deb_dkms/dkms-patchmodule.outtree.sh $BUILD_ROOT_DIR/$PKGNAME-$PKGVERSION/dkms-patchmodule.sh
+  cp $PKGBUILD_ROOT/scripts/templates/deb_dkms/prepkernel.sh $BUILD_ROOT_DIR/$PKGNAME-$PKGVERSION/
+  chmod a+x $BUILD_ROOT_DIR/$PKGNAME-$PKGVERSION/prepkernel.sh
+fi
+
+sed -e 's/\/updates/\/updates\/dkms/' $BUILD_ROOT_DIR/$PKGNAME-$PKGVERSION/dkms.conf
+
+dkms build --dkmstree $BUILD_ROOT_DIR --sourcetree $BUILD_ROOT_DIR $DKMS_KERNEL_STRING $DKMS_MODULE
+if [ $? -gt 0 ]
+then
+  echo "${RED}Error: problem during dkms build${NORMAL}"
+  exit 1
+fi
+
+# 2. packed it with fpm:
+# (wanted multiple arch deb which isn't possible with dkms and wanted to prevent install deps of dkms and related)
+
+# create deb postinstall and afterremove scripts based on template:
+rbl_dkms_apply_template $PKGBUILD_ROOT/scripts/templates/deb_dkms/postinstall.sh $BUILD_ROOT_DIR/postinstall.sh
+rbl_dkms_apply_template $PKGBUILD_ROOT/scripts/templates/deb_dkms/afterremove.sh $BUILD_ROOT_DIR/afterremove.sh
+
+# place build modules in the correct file tree
+rbl_dkms_grab_modules
+mkdir -p $BUILD_ROOT_DIR/etc//modprobe.d
+cp $BASE_DIR/motu-avb.conf $BUILD_ROOT_DIR/etc/modprobe.d/
+
+# build the package
+fpm -s dir -t deb -n $PKGNAME-${KERNEL_VER} -v $PKGVERSION \
+--license GPLv3 \
+--category misc \
+-S moode \
+--iteration $DEBVER$DEBLOC \
+--deb-priority optional \
+--url https://github.com/aircrack-ng/rtl8812au \
+-m $DEBEMAIL \
+--description "RTL8812AU/21AU Wireless drivers" \
+--post-install $BUILD_ROOT_DIR/postinstall.sh \
+--after-remove  $BUILD_ROOT_DIR/afterremove.sh  \
+$BUILD_ROOT_DIR/lib/=/lib/. \
+$BUILD_ROOT_DIR/etc/=/etc/.
+
+
+#------------------------------------------------------------
+rbl_move_to_dist
+
+echo "done"
