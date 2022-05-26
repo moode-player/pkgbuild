2362e05bc8cc35fb5e1001690d9e42a02c9c31b8
bitkeeper <bitkeeper@users.noreply.github.com>
2022-05-26 20:24:32 +0100

    [librespot] bump to 0.4.1

--- packages/librespot/build.sh
+++ packages/librespot/build.sh
@@ -13,7 +13,7 @@
 PKG="librespot_0.3.1-1moode2"
 
 PKG_SOURCE_GIT="https://github.com/librespot-org/librespot.git"
-PKG_SOURCE_GIT_TAG="v0.3.1"
+PKG_SOURCE_GIT_TAG="v0.4.1"
 
 rbl_check_cargo
 rbl_prepare_clone_from_git $PKG_SOURCE_GIT $PKG_SOURCE_GIT_TAG
@@ -30,12 +30,12 @@ echo "revision=\"$DEBVER$DEBLOC\"" >> Cargo.toml
 # Build it for V6 and higher arch (else it doesn't runs on the P2 and less)
 if [ $ARCH64 -eq 1 ]
 then
-    rustup default stable-aarch64-unknown-linux-gnu
+    # rustup default stable-aarch64-unknown-linux-gnu
     RUSTFLAGS='-Ccodegen-units=1' cargo-deb -- --features alsa-backend
 else
-    rustup default stable-arm-unknown-linux-gnueabihf
+    # rustup default stable-arm-unknown-linux-gnueabihf
     RUSTFLAGS='-Ccodegen-units=1 -Ctarget-feature=+v6,+vfp2' cargo-deb -- --features alsa-backend
-    rustup default stable-armv7-unknown-linux-gnueabihf
+    # rustup default stable-armv7-unknown-linux-gnueabihf
 fi
 
 if [[ $? -gt 0 ]]
