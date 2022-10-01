f077936fff367fc6aa7f88a4d9f2ff113c6d7409
bitkeeper <bitkeeper@users.noreply.github.com>
2022-10-01 10:48:18 +0200

    [shairport-sync] update build procedure to support 4.1.x branch with airplay 2 support.
    consequence is that we need to switch from downport from an upstream package, to using git 4.1+ debian dir from to prev version.

--- packages/shairport-sync/build.sh
+++ packages/shairport-sync/build.sh
@@ -10,22 +10,34 @@
 
 . ../../scripts/rebuilder.lib.sh
 
-PKG="shairport-sync_3.3.8-1moode1"
-PKG_DSC_URL="http://deb.debian.org/debian/pool/main/s/shairport-sync/shairport-sync_3.3.8-1.dsc"
+GIT_BASH=97fa75e8
+PKG="shairport-sync_4.1.0~git20220930.$GIT_BASH-1moode1"
 
-rbl_prepare_from_dsc_url $PKG_DSC_URL
+PKG_SOURCE_GIT="https://github.com/mikebrady/shairport-sync.git"
+PKG_SOURCE_GIT_TAG="development"
 
+PKG_DEBIAN="http://deb.debian.org/debian/pool/main/s/shairport-sync/shairport-sync_3.3.8-1.debian.tar.xz"
+
+
+rbl_prepare_from_git_with_deb_repo
+git checkout $GIT_BASH
+rbl_create_git_archive $PKG_SOURCE_GIT_TAG ../${PKGNAME}_${PKGVERSION}.tar.gz
 
 #------------------------------------------------------------
 # Custom part of the packing
 
-# patch and add patch to debian
-# patch -p1 < $BASE_DIR/debian.rules.patch
+# grab debian dir of older version
+rbl_grab_debian_archive $PKG_DEBIAN
 
-# set debian local suffix flag
-DEBFULLNAME=$DEBFULLNAME DEBEMAIL=$DEBEMAIL dch --local $DEBSUFFIX "Rebuild for moOde bullseye."
+rm -f debian/patches/github-1314.patch
+echo "" > debian/patches/series
+
+patch -p1 < $BASE_DIR/debian.rules.patch
+patch -p1 < $BASE_DIR/debian.control.patch
+
+
+DEBFULLNAME=$DEBFULLNAME DEBEMAIL=$DEBEMAIL dch --newversion $FULL_VERSION "Build for moOde."
 
 #------------------------------------------------------------
-
 rbl_build
 echo "done"
