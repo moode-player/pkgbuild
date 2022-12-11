993348b086efb59bbcd335757933393dad29e540
bitkeeper <bitkeeper@users.noreply.github.com>
2022-12-11 21:32:06 +0100

    [shareport-sync] bump to official version 4.1.1

--- packages/shairport-sync/build.sh
+++ packages/shairport-sync/build.sh
@@ -10,21 +10,15 @@
 
 . ../../scripts/rebuilder.lib.sh
 
-# set hash to match 4.1-rc2
-GIT_HASH=e7c6c4b
-PKG="shairport-sync_4.1.0~git20221009.$GIT_HASH-1moode1"
+PKG="shairport-sync_4.1.1-1moode1"
 
 PKG_SOURCE_GIT="https://github.com/mikebrady/shairport-sync.git"
-PKG_SOURCE_GIT_TAG="development"
+PKG_SOURCE_GIT_TAG="4.1.1"
 
 PKG_DEBIAN="http://deb.debian.org/debian/pool/main/s/shairport-sync/shairport-sync_3.3.8-1.debian.tar.xz"
 
 
 rbl_prepare_from_git_with_deb_repo
-rm ../*.orig.tar.gz
-git checkout -b dev-4.1rc2 $GIT_HASH
-rbl_create_git_archive $GIT_HASH ../${PKGNAME}_${PKGVERSION}.orig.tar.gz
-
 
 #------------------------------------------------------------
 # Custom part of the packing
