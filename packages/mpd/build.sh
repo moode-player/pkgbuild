83e46b3bee8429775dae868a9dbbb8bdd41ad380
bitkeeper <bitkeeper@users.noreply.github.com>
2022-12-14 20:01:35 +0100

    [mpd] use rb_patch instead of patch (abort script on error)

--- packages/mpd/build.sh
+++ packages/mpd/build.sh
@@ -18,10 +18,10 @@ rbl_prepare_from_dsc_url $PKG_DSC_URL
 #------------------------------------------------------------
 # Custom part of the packing
 
-patch -p1 < $BASE_DIR/mpd_0.23.xx_selective_resample_mode.patch
+rbl_patch $BASE_DIR/mpd_0.23.xx_selective_resample_mode.patch
 EDITOR=/bin/true dpkg-source --commit . selective_resample_mode.patch
 
-patch -p1 < $BASE_DIR/moode_build_options.patch
+rbl_patch $BASE_DIR/moode_build_options.patch
 
 # update the packageversion + debian version part
 DEBFULLNAME=$DEBFULLNAME DEBEMAIL=$DEBEMAIL dch -v $FULL_VERSION "Support for selective resample mode"
