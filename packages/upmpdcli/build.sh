b2a48322f89a7d69cfabe40e111b0eea2ca148e4
Tim Curtis <tim@moodeaudio.org>
2023-03-26 07:02:43 -0400

    Bump to upmpdcli v1.7.7

--- packages/upmpdcli/build.sh
+++ packages/upmpdcli/build.sh
@@ -11,10 +11,10 @@
 . ../../scripts/rebuilder.lib.sh
 
 
-PKG="upmpdcli_1.7.6-1moode1"
+PKG="upmpdcli_1.7.7-1moode1"
 
 PKG_SOURCE_GIT="https://framagit.org/medoc92/upmpdcli.git"
-PKG_SOURCE_GIT_TAG="upmpdcli-v1.7.6"
+PKG_SOURCE_GIT_TAG="upmpdcli-v1.7.7"
 
 rbl_prepare_from_git_with_deb_repo
 
