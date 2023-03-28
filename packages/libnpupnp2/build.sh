a481d17e00784363195edb163b4a0550895b6ccd
bitkeeper <bitkeeper@users.noreply.github.com>
2023-03-28 20:31:49 +0200

    [libnpupno2] bump to libnpupnp2_5.0.1-1moode1

--- packages/libnpupnp2/build.sh
+++ packages/libnpupnp2/build.sh
@@ -11,10 +11,10 @@
 . ../../scripts/rebuilder.lib.sh
 
 
-PKG="libnpupnp1_4.1.1-1moode1"
+PKG="libnpupnp2_5.0.1-1moode1"
 
 PKG_SOURCE_GIT="https://framagit.org/medoc92/npupnp.git"
-PKG_SOURCE_GIT_TAG="libnpupnp-v4.1.1"
+PKG_SOURCE_GIT_TAG="libnpupnp-v5.0.1"
 
 rbl_prepare_from_git_with_deb_repo
 
