4beb391b8e0b1f8f6532d600e0fef6d4fff9d66c
bitkeeper <bitkeeper@users.noreply.github.com>
2023-09-30 15:55:30 +0200

    [nqptp] package build fix for 1.2.4

--- packages/nqptp/build.sh
+++ packages/nqptp/build.sh
@@ -22,11 +22,12 @@ rbl_create_git_archive $PKG_SOURCE_GIT_TAG ../${PKGNAME}_${PKGVERSION}.tar.gz
 
 dh_make -l -p ${PKGNAME} -f ../${PKGNAME}_${PKGVERSION}.tar.gz -c custom --copyrightfile ../LICENSE -y
 rm ../${PKGNAME}_${PKGVERSION}.tar.gz
-cp $BASE_DIR/nqptp.service debian/
+ cp $BASE_DIR/nqptp.service debian/
 
 rbl_fix_control_patch_maintainer $BASE_DIR/debian.control.patch $BUILD_ROOT_DIR/debian.control.patch
-rbl_patch $BASE_DIR/debian.control.patch
-
+rbl_patch $BUILD_ROOT_DIR/debian.control.patch
+rbl_patch $BASE_DIR/skip_install_exe_hook.patch
+EDITOR=/bin/true dpkg-source --commit . skip_install_exe_hook.patch
 rbl_set_initial_version_changelog $PKGNAME $FULL_VERSION
 
 #------------------------------------------------------------
