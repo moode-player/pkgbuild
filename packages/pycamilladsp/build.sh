5db61ead25155645dca3178e30e64d28e343535a
bitkeeper <bitkeeper@users.noreply.github.com>
2023-10-28 18:17:21 +0200

    [pycamilladsp] prepare build for version 2.0.0a2, swtich to use pyproject.toml for deb build

--- packages/pycamilladsp/build.sh
+++ packages/pycamilladsp/build.sh
@@ -3,25 +3,29 @@
 #
 # Build recipe for pycamilladsp debian package
 #
-# (C) bitkeeper 2021 http://moodeaudio.org
+# (C) bitkeeper 2021 http://moodeaudio.org distutils version
+# (C) bitkeeper 2023 http://moodeaudio.org pyproject.toml version
 # License: GPLv3
 #
 #########################################################################
 
 . ../../scripts/rebuilder.lib.sh
 
-
-PKG="pycamilladsp_1.0.0-1moode1"
+# don't forget to change the debian/changelog on version bump!
+PKG="pycamilladsp_2.0.0-1moode1~a2"
 
 PKG_SOURCE_GIT="https://github.com/HEnquist/pycamilladsp.git"
-PKG_SOURCE_GIT_TAG="v1.0.0"
+PKG_SOURCE_GIT_TAG="v2.0.0-alpha2"
 
-rbl_build_py_from_git
+rbl_check_build_dep dh-python
+
+rbl_prepare_clone_from_git $PKG_SOURCE_GIT $PKG_SOURCE_GIT_TAG
+rbl_create_git_archive $PKG_SOURCE_GIT_TAG ../${PKGNAME}_${PKGVERSION}.orig.tar.gz
 
 #------------------------------------------------------------
 # Custom part of the packing
-
+cp -raf ${BASE_DIR}/debian ./debian
 
 #-----------------------------------------------------------
+rbl_build
 echo "done"
-
