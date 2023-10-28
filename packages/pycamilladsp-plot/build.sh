85fec78678ed49478fc30b0a7eb957edf2944902
bitkeeper <bitkeeper@users.noreply.github.com>
2023-10-28 18:38:54 +0200

    [pycamilladsp-plot] prepare build for version 2.0.0a3, swtich to use pyproject.toml for deb build

--- packages/pycamilladsp-plot/build.sh
+++ packages/pycamilladsp-plot/build.sh
@@ -10,18 +10,21 @@
 
 . ../../scripts/rebuilder.lib.sh
 
-PKG="pycamilladsp-plot_1.0.2-1moode1"
+PKG="pycamilladsp-plot_2.0.0-1moode1~a3"
 
 PKG_SOURCE_GIT="https://github.com/HEnquist/pycamilladsp-plot.git"
-PKG_SOURCE_GIT_TAG="v1.0.2"
+PKG_SOURCE_GIT_TAG="v2.0.0-alpha3"
+
 
 rbl_check_build_dep dh-python
-rbl_build_py_from_git
+
+rbl_prepare_clone_from_git $PKG_SOURCE_GIT $PKG_SOURCE_GIT_TAG
+rbl_create_git_archive $PKG_SOURCE_GIT_TAG ../${PKGNAME}_${PKGVERSION}.orig.tar.gz
 
 #------------------------------------------------------------
 # Custom part of the packing
-
+cp -raf ${BASE_DIR}/debian ./debian
 
 #-----------------------------------------------------------
+rbl_build
 echo "done"
-
