d000eb82e6132fe92cb20be24edd35df17f250ae
bitkeeper <bitkeeper@users.noreply.github.com>
2024-09-14 14:12:02 +0200

    [libnpupnp] bump to 6.2.0

--- packages/libnpupnp/build.sh
+++ packages/libnpupnp/build.sh
@@ -11,10 +11,10 @@
 . ../../scripts/rebuilder.lib.sh
 
 
-PKG="libnpupnp_6.1.2-1moode1"
+PKG="libnpupnp_6.2.0-1moode1"
 
 PKG_SOURCE_GIT="https://framagit.org/medoc92/npupnp.git"
-PKG_SOURCE_GIT_TAG="libnpupnp-v6.1.2"
+PKG_SOURCE_GIT_TAG="libnpupnp-v6.2.0"
 
 rbl_prepare_from_git_with_deb_repo
 
