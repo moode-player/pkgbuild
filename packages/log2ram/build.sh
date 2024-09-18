e3eae01f7b8d20c7a5587a1910ac770690005787
bitkeeper <bitkeeper@users.noreply.github.com>
2024-09-18 18:55:31 +0200

    [log2ram] add initial package build script

---
+++ packages/log2ram/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#########################################################################
+#
+# Build recipe for log2ram debian package
+#
+# (C) bitkeeper 2024 http://moodeaudio.org
+# License: GPLv3
+#
+#########################################################################
+
+. ../../scripts/rebuilder.lib.sh
+
+PKG="log2ram_1.7.2-1moode1"
+
+PKG_SOURCE_GIT="https://github.com/azlux/log2ram.git"
+PKG_SOURCE_GIT_TAG="1.7.2"
+
+rbl_check_build_dep jq
+rbl_prepare_clone_from_git $PKG_SOURCE_GIT
+
+#------------------------------------------------------------
+# Custom part of the packing
+./build-packages.sh
+
+#------------------------------------------------------------
+# rbl_build
+mkdir -p $BASE_DIR/dist/binary
+mv ./deb/log2ram_*.deb $BASE_DIR/dist/binary
+echo "done"
