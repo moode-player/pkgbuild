a334735477105ad26db3c241ce9b0c81bfbcaee6
bitkeeper <bitkeeper@users.noreply.github.com>
2024-11-02 11:27:43 +0100

    [librespot] fixe setting pkg revision and cleanup of build script

--- packages/librespot/build.sh
+++ packages/librespot/build.sh
@@ -14,11 +14,10 @@ PKG="librespot_0.6.0-1moode1"
 
 PKG_SOURCE_GIT="https://github.com/librespot-org/librespot.git"
 PKG_SOURCE_GIT_TAG="v0.6.0"
-DEBSUFFIXVERSION=2
 
 rbl_check_cargo
-rbl_prepare_clone_from_git $PKG_SOURCE_GIT $PKG_SOURCE_GIT_TAG
-rbl_create_git_archive $PKG_SOURCE_GIT_TAG ../${PKGNAME}_${PKGVERSION}.orig.tar.gz
+rbl_prepare_clone_from_git ${PKG_SOURCE_GIT} ${PKG_SOURCE_GIT_TAG}
+rbl_create_git_archive ${PKG_SOURCE_GIT_TAG} ../${PKGNAME}_${PKGVERSION}.orig.tar.gz
 
 # ------------------------------------------------------------
 # Custom part of the packing
@@ -26,22 +25,19 @@ rbl_create_git_archive $PKG_SOURCE_GIT_TAG ../${PKGNAME}_${PKGVERSION}.orig.tar.
 rbl_check_build_dep libasound2-dev
 
 #Add to [package.metadata.deb] section of Cargo.toml:
-echo "revision=\"$DEBVER$DEBLOC\"" >> Cargo.toml
-
-# Build it for V6 and higher arch (else it doesn't runs on the P2 and less)
-if [ $ARCH64 -eq 1 ]
+sed -i "s/^priority = \"optional\"/priority = \"optional\"\nrevision = \"${DEBVER}${DEBLOC}\"/" Cargo.toml
+if [[ $? -gt 0 ]]
 then
-    # rustup default stable-aarch64-unknown-linux-gnu
-    RUSTFLAGS='-Ccodegen-units=1' cargo-deb -- --features alsa-backend
-else
-    rustup default stable-arm-unknown-linux-gnueabihf
-    RUSTFLAGS='-Ccodegen-units=1 -Ctarget-feature=+v6,+vfp2' cargo-deb -- --features alsa-backend
-    rustup default stable-armv7-unknown-linux-gnueabihf
+    echo "${RED}Error: sed failed to set correct PKG VERSION!${NORMAL}"
+    exit
 fi
+
+# rustup default stable-aarch64-unknown-linux-gnu
+RUSTFLAGS='-Ccodegen-units=1' cargo-deb -- --features alsa-backend
 
 if [[ $? -gt 0 ]]
 then
-    echo "${RED}Error: cargo-deb failed during build"
+    echo "${RED}Error: cargo-deb failed during build${NORMAL}"
     exit
 fi
 
