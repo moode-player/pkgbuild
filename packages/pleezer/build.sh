af2340135f5812331281fca38100de39f42a6ddf
bitkeeper <bitkeeper@users.noreply.github.com>
2024-11-23 17:33:56 +0100

    [pleezer] add initial build script, version 0.2.0

---
+++ packages/pleezer/build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#########################################################################
+#
+# Build recipe for pleezer debian package
+#
+# (C) bitkeeper 2024 http://moodeaudio.org
+# License: GPLv3
+#
+#########################################################################
+
+. ../../scripts/rebuilder.lib.sh
+
+PKG="pleezer_0.2.0-1moode1"
+
+PKG_SOURCE_GIT="https://github.com/roderickvd/pleezer.git"
+PKG_SOURCE_GIT_TAG="v0.2.0"
+
+rbl_check_cargo
+rbl_prepare_clone_from_git ${PKG_SOURCE_GIT} ${PKG_SOURCE_GIT_TAG}
+rbl_create_git_archive ${PKG_SOURCE_GIT_TAG} ../${PKGNAME}_${PKGVERSION}.orig.tar.gz
+
+# ------------------------------------------------------------
+# Custom part of the packing
+
+#Add to [package.metadata.deb] section of Cargo.toml:
+# sed -i "s/^priority = \"optional\"/priority = \"optional\"\nrevision = \"${DEBVER}${DEBLOC}\"/" Cargo.toml
+# if [[ $? -gt 0 ]]
+# then
+#     echo "${RED}Error: sed failed to set correct PKG VERSION!${NORMAL}"
+#     exit
+# fi
+echo -e "\n[package.metadata.deb]\n\
+revision = \"${DEBVER}${DEBLOC}\" \
+ " >> Cargo.toml
+
+
+RUSTFLAGS='-Ccodegen-units=1' cargo-deb --
+
+if [[ $? -gt 0 ]]
+then
+    echo "${RED}Error: cargo-deb failed during build${NORMAL}"
+    exit
+fi
+
+mv target/debian/pleezer*.deb ..
+/build/pleezer-0.2.0/target/debian/pleezer_0.2.0_arm64.deb
+#------------------------------------------------------------
+# post_build
+rbl_move_to_dist
+
+echo "done"
