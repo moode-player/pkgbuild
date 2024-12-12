94d6c73ac2d389b6dc2fff18aa1765361b246946
Tim Curtis <tim@moodeaudio.org>
2024-12-12 05:21:39 -0500

    [postinstall] Remove "Set ownership on homedir/.config"
    The .config dir won't exist yet at this part of the install so lets set ownership in worker startup.

--- packages/moode-player/postinstall.sh
+++ packages/moode-player/postinstall.sh
@@ -152,9 +152,6 @@ function on_install() {
     touch /var/log/php_errors.log
     chmod 0666 /var/log/php_errors.log
 
-    echo "** Set ownership on homedir/.config"
-    chown pi:pi /home/pi/.config/
-
     echo "** Set permissions on homedir scripts"
     chmod 0755 /home/pi/*.sh
 
