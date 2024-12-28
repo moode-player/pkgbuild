0487bfaa2ad7028260a3dec43f67e5d5d6372b23
Tim Curtis <tim@moodeaudio.org>
2024-12-28 07:07:48 -0500

    [pleezer] Bump to 0.7.0

--- packages/pleezer/build.sh
+++ packages/pleezer/build.sh
@@ -9,7 +9,7 @@
 #########################################################################
 
 . ../../scripts/rebuilder.lib.sh
-VER="0.6.2"
+VER="0.7.0"
 PKG="pleezer_$VER-1moode1"
 
 PKG_SOURCE_GIT="https://github.com/roderickvd/pleezer.git"
