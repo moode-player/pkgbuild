89a38ea9d38420e94602325557ea0c65ad291b23
bitkeeper <bitkeeper@users.noreply.github.com>
2025-01-25 09:38:16 +0100

    [mpd2cdspvolime] bump to version 2.0.0 for cdsp 3.x support

--- packages/mpd2cdspvolume/build.sh
+++ packages/mpd2cdspvolume/build.sh
@@ -11,10 +11,10 @@
 . ../../scripts/rebuilder.lib.sh
 
 
-PKG="mpd2cdspvolume_1.0.0-1moode2"
+PKG="mpd2cdspvolume_2.0.0-1moode2"
 
 PKG_SOURCE_GIT="https://github.com/bitkeeper/mpd2cdspvolume.git"
-PKG_SOURCE_GIT_TAG="v1.0.0"
+PKG_SOURCE_GIT_TAG="v2.0.0"
 
 # rbl_prepare_clone_from_git $PKG_SOURCE_GIT $PKG_SOURCE_GIT_TAG
 rbl_prepare_clone_from_git $PKG_SOURCE_GIT
