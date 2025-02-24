6732a773c683ebac5af3c6e22b493dbfa6ea394c
Tim Curtis <tim@moodeaudio.org>
2025-02-24 18:31:53 -0500

    Bump to WIP r926

--- packages/moode-player/postinstall.sh
+++ packages/moode-player/postinstall.sh
@@ -745,6 +745,12 @@ function on_upgrade() {
         sed -i -e 's/\/\/[[:space:]]\+\(disable_synchronization\)/\1/' /etc/shairport-sync.conf
     fi
 
+    # Introduced in r925
+    dpkg --compare-versions $VERSION lt "9.2.6-1moode1"
+    if [ $? -eq 0 ]; then
+        echo "There are no postinstall updates for r926"
+    fi
+
     # --------------------------------------------------------------------------
     # Any release
     # --------------------------------------------------------------------------
