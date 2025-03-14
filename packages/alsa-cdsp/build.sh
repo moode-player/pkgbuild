aa6247c622a632b2cb4231f57d0ba15ff330ef34
bitkeeper <bitkeeper@users.noreply.github.com>
2025-03-14 14:28:26 +0100

    [alsa-cdsp] bump to 1.2.0-2moode1, reduce debug level from 2 to 1

--- packages/alsa-cdsp/build.sh
+++ packages/alsa-cdsp/build.sh
@@ -10,10 +10,10 @@
 
 . ../../scripts/rebuilder.lib.sh
 
-PKG="alsa-cdsp_1.2.0-1moode2"
+PKG="alsa-cdsp_1.2.0-2moode1"
 
 PKG_SOURCE_GIT="https://github.com/bitkeeper/alsa_cdsp.git"
-# PKG_SOURCE_GIT_TAG="v1.2.0"
+PKG_SOURCE_GIT_TAG="v1.2.0"
 # PKG_SOURCE_GIT_TAG="fixes/bookworm_cargs_empty"
 
 rbl_check_build_dep libasound2-dev
@@ -30,6 +30,9 @@ rbl_create_git_archive $PKG_SOURCE_GIT_TAG ../${PKGNAME}_${PKGVERSION}.tar.gz
 
 dh_make -l -p ${PKGNAME} -f ../${PKGNAME}_${PKGVERSION}.tar.gz -c custom --copyrightfile ../LICENSE -y
 rm ../${PKGNAME}_${PKGVERSION}.tar.gz
+
+rbl_patch $BASE_DIR/debug_level.patch
+EDITOR=/bin/true dpkg-source --commit . debug_level.patch
 
 rbl_patch $BASE_DIR/fix_make_clean.patch
 EDITOR=/bin/true dpkg-source --commit . fix_make_clean.patch
