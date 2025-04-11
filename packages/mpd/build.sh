b495e04f5a880a4be0937e8eb4a35eb41b5cdbb5
bitkeeper <bitkeeper@users.noreply.github.com>
2025-04-11 20:37:12 +0200

    [mpd] bump to 0.24.3 and update build process to use deb source based packages again

--- packages/mpd/build.sh
+++ packages/mpd/build.sh
@@ -15,18 +15,12 @@
 
 . ../../scripts/rebuilder.lib.sh
 
-PKG="mpd_0.24.3-1moode1"
-PKG_SOURCE_GIT="https://github.com/MusicPlayerDaemon/MPD.git"
-PKG_SOURCE_GIT_TAG="v0.24.3"
+PKG_DSC_URL="http://deb.debian.org/debian/pool/main/m/mpd/mpd_0.24.3-1.dsc"
+DEBSUFFIXVERSION=1
 
-PKG_DEBIAN="http://deb.debian.org/debian/pool/main/m/mpd/mpd_0.23.17-1.debian.tar.xz"
-rbl_prepare_from_git_with_deb_repo
-
+rbl_prepare_from_dsc_url $PKG_DSC_URL
 #------------------------------------------------------------
 # Custom part of the packing
-
-# grab debian dir of same or older version
-rbl_grab_debian_archive $PKG_DEBIAN
 
 rbl_patch $BASE_DIR/moode_build_options.patch
 rbl_patch $BASE_DIR/debian.control.patch
