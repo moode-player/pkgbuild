8bc55b364b7fffe3f9b92e99e5ef19db95192c4f
bitkeeper <bitkeeper@users.noreply.github.com>
2025-04-13 10:59:53 +0200

    [ shairport-sync-metadata-reader] updated copyrights etc

--- packages/shairport-sync-metadata-reader/build.sh
+++ packages/shairport-sync-metadata-reader/build.sh
@@ -10,16 +10,17 @@
 
 . ../../scripts/rebuilder.lib.sh
 
-#TODO: If needed add systemd files
-
-PKG="shairport-sync-metadata-reader_1.0.0-1moode1"
+GIT_HASH=9caf251
+PKG="shairport-sync-metadata-reader_1.0.2~git20250413.$GIT_HASH-1moode1"
 
 PKG_SOURCE_GIT="https://github.com/mikebrady/shairport-sync-metadata-reader.git"
 PKG_SOURCE_GIT_TAG="master"
 
 
 rbl_prepare_clone_from_git $PKG_SOURCE_GIT $PKG_SOURCE_GIT_TAG
-rbl_create_git_archive $PKG_SOURCE_GIT_TAG ../${PKGNAME}_${PKGVERSION}.tar.gz
+
+git checkout -b dev $GIT_HASH
+rbl_create_git_archive $GIT_HASH ../${PKGNAME}_${PKGVERSION}.tar.gz
 
 #------------------------------------------------------------
 # Custom part of the packing
@@ -27,13 +28,12 @@ rbl_create_git_archive $PKG_SOURCE_GIT_TAG ../${PKGNAME}_${PKGVERSION}.tar.gz
 dh_make -s -p ${PKGNAME} -f ../${PKGNAME}_${PKGVERSION}.tar.gz -y
 rm ../${PKGNAME}_${PKGVERSION}.tar.gz
 
-# TODO: customize the to set correct author and hompage
+
 #rbl_fix_control_patch_maintainer $BASE_DIR/debian.control.patch $BUILD_ROOT_DIR/debian.control.patch
-#rbl_patch $BUILD_ROOT_DIR/debian.control.patch
+rbl_patch $BASE_DIR/debian-copyright.patch
 rm debian/manpage.*.ex
 rm debian/README.*
 
-# DEBFULLNAME=$DEBFULLNAME DEBEMAIL=$DEBEMAIL dch --newversion $FULL_VERSION "Modifications and enhancements to support integration into moOde audio player. Mods by Tim Curtis tim@moodeaudio.org"
 rbl_set_initial_version_changelog $PKGNAME $FULL_VERSION
 
 #------------------------------------------------------------
