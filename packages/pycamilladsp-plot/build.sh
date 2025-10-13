df8ac0b6b181ad5e773652cd9a44a4d9ea6f8ee6
bitkeeper <bitkeeper@users.noreply.github.com>
2025-10-13 20:41:30 +0200

    [pycamilladsp] FIx broken build by adding build dep

--- packages/pycamilladsp-plot/build.sh
+++ packages/pycamilladsp-plot/build.sh
@@ -21,6 +21,7 @@ PKG_SOURCE_GIT="https://github.com/HEnquist/pycamilladsp-plot.git"
 PKG_SOURCE_GIT_TAG="v3.0.0"
 
 rbl_check_build_dep dh-python
+rbl_check_build_dep pybuild-plugin-pyproject
 
 rbl_prepare_clone_from_git $PKG_SOURCE_GIT $PKG_SOURCE_GIT_TAG
 rbl_create_git_archive $PKG_SOURCE_GIT_TAG ../${PKGNAME}_${PKGVERSION}.orig.tar.gz
