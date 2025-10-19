949f6c740e0726469143d8651c99b3bcfcce1553
Tim Curtis <tim@moodeaudio.org>
2025-10-19 07:42:01 -0400

    [spectrum.py] Add copyright and comment

--- packages/peppy-spectrum/spectrum.py
+++ packages/peppy-spectrum/spectrum.py
@@ -14,6 +14,11 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Peppy Player. If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2025 The moOde audio player project / Tim Curtis
+# - Apply patch described in the issue below plus add debug logging
+# - https://github.com/project-owner/PeppySpectrum/issues/1
+#
 
 import pygame
 import time
