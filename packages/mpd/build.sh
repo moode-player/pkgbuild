35cea48235af5f49112d24606264048a7ed1d3c7
Tim Curtis <tim@moodeaudio.org>
2025-10-22 17:55:11 -0400

    Update build.sh

--- packages/mpd/build.sh
+++ packages/mpd/build.sh
@@ -15,9 +15,9 @@
 
 . ../../scripts/rebuilder.lib.sh
 
-PKG="mpd_0.24.5-1moode1"
+PKG="mpd_0.24.6-1moode1"
 PKG_SOURCE_GIT="https://github.com/MusicPlayerDaemon/MPD.git"
-PKG_SOURCE_GIT_TAG="v0.24.5"
+PKG_SOURCE_GIT_TAG="v0.24.6"
 
 PKG_DEBIAN="http://deb.debian.org/debian/pool/main/m/mpd/mpd_0.24.4-1.debian.tar.xz"
 rbl_prepare_from_git_with_deb_repo
