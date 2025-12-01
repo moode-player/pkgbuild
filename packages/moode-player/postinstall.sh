21981f779d1e92a9febdf4f4d232c933daeeab80
Tim Curtis <tim@moodeaudio.org>
2025-12-01 12:24:38 -0500

    [postinstall] Improve postinstall message

--- packages/moode-player/postinstall.sh
+++ packages/moode-player/postinstall.sh
@@ -481,7 +481,8 @@ function on_upgrade() {
 	# Introduced in r1001
 	dpkg --compare-versions $VERSION lt "10.0.1-1moode1"
 	if [ $? -eq 0 ]; then
-		echo "There are no postinstall updates for r1001"
+		#echo "There are no postinstall updates for r1001"
+		echo "Apply postinstall updates for r1001"
 		# Hide feature Deezer Connect
 		sqlite3 $SQLDB "UPDATE cfg_system SET value='228279' WHERE param='feat_bitmask'"
 	fi
