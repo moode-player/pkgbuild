4bebc59d981a12d4da86c56c513c3715f1c53008
Tim Curtis <tim@moodeaudio.org>
2026-05-28 17:35:36 -0400

    [libupnpp-bindings] Fix package version

--- packages/libupnpp-bindings/build.sh
+++ packages/libupnpp-bindings/build.sh
@@ -11,7 +11,7 @@
 
 . ../../scripts/rebuilder.lib.sh
 
-PKG="libupnpp-bindings_1.26.1-1moode1"
+PKG="libupnpp-bindings_0.26.1-1moode1"
 
 PKG_SOURCE_GIT="https://framagit.org/medoc92/libupnpp-bindings.git"
 PKG_SOURCE_GIT_TAG="libupnpp-bindings-v0.26.1"
