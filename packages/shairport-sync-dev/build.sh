6f99054ee16d567365877394c6cca813aa538901
Tim Curtis <tim@moodeaudio.org>
2026-07-28 18:46:35 -0400

    [shairport-sync-dev] build.sh updates

--- packages/shairport-sync-dev/build.sh
+++ packages/shairport-sync-dev/build.sh
@@ -10,12 +10,12 @@
 
 . ../../scripts/rebuilder.lib.sh
 
-PKG="shairport-sync_5.2-1moode1"
+PKG="shairport-sync-dev_5.2-1moode1"
 
 PKG_SOURCE_GIT="https://github.com/mikebrady/shairport-sync.git"
 PKG_SOURCE_GIT_TAG="5.2"
 
-PKG_DEBIAN="http://deb.debian.org/debian/pool/main/s/shairport-sync/shairport-sync_4.3.7-1.debian.tar.xz"
+PKG_DEBIAN="http://deb.debian.org/debian/pool/main/s/shairport-sync/shairport-sync_5.1~dev~git20260518-1.debian.tar.xz"
 
 rbl_check_build_dep libplist-utils
 rbl_prepare_from_git_with_deb_repo
