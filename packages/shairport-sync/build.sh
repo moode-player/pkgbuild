92e4a57a98d1e4814e6f9c39fb4b8ec99d39f65d
Tim Curtis <tim@moodeaudio.org>
2026-08-04 09:52:19 -0400

    Revert "[shairport-sync] Bump to 5.2.1"
    This reverts commit a58e0c55edaaf44f8a6367461fbb55c83efafd42.

--- packages/shairport-sync/build.sh
+++ packages/shairport-sync/build.sh
@@ -10,12 +10,12 @@
 
 . ../../scripts/rebuilder.lib.sh
 
-PKG="shairport-sync_5.2.1-1moode1"
+PKG="shairport-sync_5.0.4-1moode1"
 
 PKG_SOURCE_GIT="https://github.com/mikebrady/shairport-sync.git"
-PKG_SOURCE_GIT_TAG="5.2.1"
+PKG_SOURCE_GIT_TAG="5.0.4"
 
-PKG_DEBIAN="http://deb.debian.org/debian/pool/main/s/shairport-sync/shairport-sync_5.1~dev~git20260518-1.debian.tar.xz"
+PKG_DEBIAN="http://deb.debian.org/debian/pool/main/s/shairport-sync/shairport-sync_4.3.7-1.debian.tar.xz"
 
 rbl_check_build_dep libplist-utils
 rbl_prepare_from_git_with_deb_repo
@@ -26,7 +26,12 @@ rbl_prepare_from_git_with_deb_repo
 # grab debian dir of older version
 rbl_grab_debian_archive $PKG_DEBIAN
 
+rm -f debian/patches/github-1314.patch
+echo "" > debian/patches/series
+
 rbl_patch $BASE_DIR/debian.rules.patch
+rbl_patch $BASE_DIR/debian.control.patch
+
 
 DEBFULLNAME=$DEBFULLNAME DEBEMAIL=$DEBEMAIL dch --newversion $FULL_VERSION "Build for moOde."
 
